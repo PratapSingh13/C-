347cd05c5053cbbc1178809727faec256b42bca9
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2020-11-03 13:22:24 +0530

    A file to print natural numbers

---
+++ naturalNumbers.cs
@@ -0,0 +1,13 @@
+// A C# program to print numbers from 1 to 10.
+using System;
+namespace naturalNumbers
+{
+    public class numbers
+    {
+        public static void Main()
+        {
+            for(int i = 1; i <= 10; i++)
+                Console.WriteLine(i);
+        }
+    }
+}
