b602d5c7ecb2891d9b3d10d2ac33dea2c8ed720e
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2020-11-03 13:14:04 +0530

    A file for Nested FOR loop

---
+++ nestedForLoop.cs
@@ -0,0 +1,14 @@
+// C# Program to illustrate nested for loop
+using System;
+namespace nestedForLoop
+{
+    public class ABC
+    {
+        public static void Main()
+        {
+            for(int i = 0; i < 4; i++)
+                for(int j = 1; j < i; j++)
+                    Console.WriteLine("Yogendra Pratap Singh");
+        }
+    }
+}
