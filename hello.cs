502c2c4ca768de9b3a5368032036db0a04b9436b
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2020-11-03 08:23:37 +0530

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,11 @@
+// A C# Programm for Hello World!
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
